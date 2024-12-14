4029ec7b0b690425f1f90748e08e867df55a0185
Theologos Batsioulas <tbatsioulas@deloitte.gr>
2024-12-15 00:48:30 +0200

    Initial Commit

---
+++ YourHotel/YourHotelApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourHotelApp.swift
+//  YourHotel
+//
+//  Created by Batsioulas, Theologos on 15/12/24.
+//
+
+import SwiftUI
+
+@main
+struct YourHotelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
