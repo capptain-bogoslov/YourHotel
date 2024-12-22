0859ee07b007d590b09da5e364a510368ee08d1f
Theologos Batsioulas <tbatsioulas@deloitte.gr>
2024-12-22 13:56:11 +0200

    Add bottom navigation with gestures and feedback generator - Add strings catalogue

--- YourHotel/Views/HomeView.swift
+++ YourHotel/Views/HomeView.swift
@@ -8,11 +8,149 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State var tabSelected: Int = 1
+    let minDragTranslationForSwipe: CGFloat = 50
+    private let impactFeedbackGenerator = UIImpactFeedbackGenerator(style: .medium)
+
+
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView(selection: $tabSelected) {
+            Group {
+
+                ZStack {
+                    Color.primaryColor.ignoresSafeArea()
+                    HomeContent()
+                    
+                }
+//                .onTapGesture {
+//                    if self.isDrawerOpen {
+//                        self.isDrawerOpen.toggle()
+//                    }
+//                }
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("home")
+                }
+                .tag(1)
+                .simultaneousGesture(DragGesture().onEnded({ handleSwipe(translation: $0.translation.width)
+                }))
+//                                .highPriorityGesture(DragGesture().onEnded({ handleSwipe(translation: $0.translation.width)
+//                                }))
+                .onChange(of: tabSelected) { _ in
+                    impactFeedbackGenerator.impactOccurred()
+                }
+                
+                ZStack {
+                    Color.secondary.ignoresSafeArea()
+                    HomeContent()
+                    
+                }
+//                .onTapGesture {
+//                    if self.isDrawerOpen {
+//                        self.isDrawerOpen.toggle()
+//                    }
+//                }
+                .tabItem {
+                    Image(systemName: "building.2.fill")
+                    Text("hotel")
+                }
+                .tag(1)
+                .simultaneousGesture(DragGesture().onEnded({ handleSwipe(translation: $0.translation.width)
+                }))
+//                                .highPriorityGesture(DragGesture().onEnded({ handleSwipe(translation: $0.translation.width)
+//                                }))
+                .onChange(of: tabSelected) { _ in
+                    impactFeedbackGenerator.impactOccurred()
+                }
+                
+                
+                ZStack {
+                    Color.red.ignoresSafeArea()
+                    HomeContent()
+                }
+//                .onTapGesture {
+//                    if self.isDrawerOpen {
+//                        self.isDrawerOpen.toggle()
+//                    }
+//                }
+                .tabItem {
+                    Image(systemName: "calendar")
+                    Text("calendar")
+                }
+                .tag(2)
+                .simultaneousGesture(DragGesture().onEnded({ handleSwipe(translation: $0.translation.width)
+                }))
+
+                .onChange(of: tabSelected) { _ in
+                    impactFeedbackGenerator.impactOccurred()
+                }
+                
+                ZStack {
+                    Color.yellow.ignoresSafeArea()
+                    HomeContent()
+                }
+//                .onTapGesture {
+//                    if self.isDrawerOpen {
+//                        self.isDrawerOpen.toggle()
+//                    }
+//                }
+                .tabItem {
+                    Image(systemName: "fork.knife")
+                    Text("services")
+                }
+                .tag(3)
+                .simultaneousGesture(DragGesture().onEnded({ handleSwipe(translation: $0.translation.width)
+                }))
+                .onChange(of: tabSelected) { _ in
+                    impactFeedbackGenerator.impactOccurred()
+                }
+                
+                ZStack {
+                    Color.tertiaryColor.ignoresSafeArea()
+                    HomeContent()
+                }
+//                .onTapGesture {
+//                    if self.isDrawerOpen {
+//                        self.isDrawerOpen.toggle()
+//                    }
+//                }
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("profile")
+                }
+                .tag(0)
+                .simultaneousGesture(DragGesture().onEnded({ handleSwipe(translation: $0.translation.width)
+                }))
+                .onChange(of: tabSelected) { _ in
+                    impactFeedbackGenerator.impactOccurred()
+                }
+                
+            }
+            .toolbarBackground(Color.primaryColor, for: .tabBar)
+            .toolbarBackground(.visible, for: .tabBar)
+        }
+        .tint(.black)
+    }
+    
+    //swipe gesture
+    private func handleSwipe(translation: CGFloat) {
+        if translation > minDragTranslationForSwipe && tabSelected > 0 {
+            tabSelected -= 1
+        } else  if translation < -minDragTranslationForSwipe && tabSelected < 3 {
+            tabSelected += 1
+        }
     }
 }
 
 #Preview {
     HomeView()
+}
+
+
+struct HomeContent: View {
+    var body: some View {
+        Text("Hello")
+    }
 }
