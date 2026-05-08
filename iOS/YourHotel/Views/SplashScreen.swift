0c4ebf6764fa6e4a17d2e2c54039f943cc1d52e8
Theo Batsioulas <tbatsioulas@deloitte.gr>
2026-05-08 21:33:50 +0300

    Add Google-Services file - add animation video - add Lottie package - implement Lottie nimation for SplashScreen

--- iOS/YourHotel/Views/SplashScreen.swift
+++ iOS/YourHotel/Views/SplashScreen.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import AVKit
+import DotLottie
 
 struct SplashScreen: View {
     @AppStorage("isDarkMode") private var isDarkMode = false
@@ -15,19 +17,60 @@ struct SplashScreen: View {
         
         if self.isActive {
             HomeView()
+                .transition(.opacity)
         } else {
-        ZStack {
-            Color.red.ignoresSafeArea()
-        
-            Text("Hello!")
-                .font(.system(size: 40))
+            ZStack {
+                Color.white.ignoresSafeArea()
+                VideoSplashView(isFinished: $isActive)
             }
+            .ignoresSafeArea()
             .onAppear {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 8.5) {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 7.5) {
                     self.isActive.toggle()
                 }
             }
             .preferredColorScheme(isDarkMode ? .dark : .light)
+        }
+    }
+}
+
+struct VideoSplashView: View {
+    @Binding var isFinished: Bool
+    private let player = AVPlayer(url: Bundle.main.url(forResource: "sonia_animation", withExtension: "mp4")!)
+    
+    // 1. Initialize the animation using your local .lottie file
+        private let animation = DotLottieAnimation(
+            fileName: "sonia_animation", // Do not include the ".lottie" extension here
+            config: AnimationConfig(autoplay: true, loop: false)
+        )
+    
+    var body: some View {
+        ZStack {
+            Color.white.ignoresSafeArea()
+//            VideoPlayer(player: player)
+//                .frame(width: 150, height:150)
+//                .onAppear {
+//                    player.play()
+//                    
+//                    // Listen for the video finishing
+//                    NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: player.currentItem, queue: .main) { _ in
+//                        withAnimation {
+//                            isFinished = true
+//                        }
+//                    }
+//                }
+//                .onDisappear {
+//                    player.pause()
+//                }
+            // 2. Display the player view and customize it using modifiers
+//                        DotLottiePlayerView(animation: animation)
+//                            .looping()
+//                            .animationSpeed(1.5) // Speeds up the animation slightly
+//                            .frame(width: 400, height: 400)
+            
+            DotLottieView(dotLottie: animation)
+                .frame(width: 400, height: 400)
+
         }
     }
 }
